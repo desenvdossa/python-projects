fb22f0aa5dc7f52aa8184ccc07b10339b6cce0fa
desenvdossa <desenvolvimentodossa@gmail.com>
2024-09-25 17:37:43 -0300

    Update sistema_bancario.py
    Adicionando travas para impossibilitar a inserção de valores negativos

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -6,24 +6,35 @@ def sacar(saldo, numero_saques):
     valor_saque = float(input("Valor a sacar: ").replace(',','.'))
     if (saldo - valor_saque < 0):
         print("Não é possível sacar esse valor, saldo insuficiente")
+        return (saldo, numero_saques, 0)
+    elif(valor_saque > 500):
+        print("O limite máximo por saque é de R$500,00")
+        return (saldo, numero_saques, 0)
+    elif(valor_saque < 0):
+        print("Valor inválido")
+        return (saldo, numero_saques, 0)
     else:
         saldo -= valor_saque
         numero_saques += 1
         print(f"Valor retirado: R${valor_saque:.2f}")
         print(f"Saldo atual: {saldo:.2f}")
         print()
-    return (saldo, numero_saques, valor_saque)
+        return (saldo, numero_saques, valor_saque)
 
 
 
 
 def depositar(saldo):
     valor_deposito = float(input("Valor a depositar: ").replace(',','.')) # converte imputs (padrão string) para int 
-    saldo += valor_deposito
-    print(f"Valor depositado: R${valor_deposito:.2f} ")
-    print(f"Saldo atual: R${saldo:.2f}") 
-    print()
-    return (saldo, valor_deposito)
+    if(valor_deposito < 0 ):
+        print("Valor inválido")
+        return (saldo, 0)
+    else:
+        saldo += valor_deposito
+        print(f"Valor depositado: R${valor_deposito:.2f} ")
+        print(f"Saldo atual: R${saldo:.2f}") 
+        print()
+        return (saldo, valor_deposito)
 
 
 
@@ -60,26 +71,28 @@ while(1):
             saldo, numero_saques, valor = sacar(saldo, numero_saques)
             print(f"Numero de saques diários: {numero_saques}")
             hora = time.asctime()
-            resultado = ("Operação: SAQUE" + " Valor: R$" + str(valor) + " Data: " + str(hora))
-            print(resultado)
-            print()
-            operacoes = operacoes + resultado + os.linesep
+            if(valor != 0):
+                resultado = ("Operação: SAQUE" + " Valor: R$" + str(valor) + " Data: " + str(hora))
+                print(resultado)
+                print()
+                operacoes = operacoes + resultado + os.linesep
 
     elif(operacao == 'D'):
       saldo, valor = depositar(saldo)
-      hora = time.asctime()
-      resultado = ("Operação: DEPÓSITO" + " Valor: " + str(valor) + " Data: " + str(hora))
-      print(resultado)
-      print()
-      operacoes = operacoes + resultado + os.linesep
+      
+      if(valor != 0):
+        hora = time.asctime()
+        resultado = ("Operação: DEPÓSITO" + " Valor: R$" + str(valor) + " Data: " + str(hora))
+        print(resultado)
+        print()
+        operacoes = operacoes + resultado + os.linesep
 
     elif(operacao == 'E'):
-        hora = time.asctime()
-        resultado = ("Operação: EXTRATO" + " Valor: 0" + " Data: " + str(hora))
-        print(resultado)
-        operacoes = operacoes + resultado + os.linesep
-        print()
-        extrato(saldo, operacoes) 
+        if(operacoes == ''):
+            print("Não foram realizadas movimentações")
+            print()
+        else:
+            extrato(saldo, operacoes) 
 
 
 
